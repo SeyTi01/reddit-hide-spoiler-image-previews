2b76e6b70fd3184fc57e42bb3749e952d4961aa3
SeyTi01 <jannis.ridder@gmail.com>
2023-09-10 15:00:15 +0200

    [REFACTOR] removed unnecessary condition, extracted BACKGROUND_DIV_SELECTOR

--- src/spoilerPreviewFix.user.js
+++ src/spoilerPreviewFix.user.js
@@ -14,6 +14,7 @@
 
     const ICON_CLASS = '_3CquMWJ6RMh8E9D-_84AtZ _2hIvPRO2xz4rn9LXAJXYDa _10qSZsDWnOBwx4bc7GJ1QF icon icon-media_gallery';
     const IMAGE_SELECTOR = 'div[aria-label][data-click-id="image"]';
+    const BACKGROUND_DIV_SELECTOR = 'div[data-click-id="background"]';
     const SPOILER_SELECTOR = 'span._1wzhGvvafQFOWAyA157okr';
 
     let div = document.createElement('div');
@@ -25,10 +26,7 @@
         let images = document.querySelectorAll(IMAGE_SELECTOR);
 
         for (let image of images) {
-            let img = image.querySelector('img._25ZOvQhQdAqwdxPd5z-KFB');
-            if (!img) continue;
-
-            let spoilerSpan = image.closest('div[data-click-id="background"]').querySelector(SPOILER_SELECTOR);
+            let spoilerSpan = image.closest(BACKGROUND_DIV_SELECTOR).querySelector(SPOILER_SELECTOR);
             if (!spoilerSpan) continue;
 
             image.replaceWith(div.cloneNode(true));
