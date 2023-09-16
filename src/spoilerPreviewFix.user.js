b587009c2f5a9effee910bed20e8fbc026566405
SeyTi01 <144541737+SeyTi01@users.noreply.github.com>
2023-09-16 23:19:03 +0200

    [RELEASE] 1.6 (#10)
    * [FEATURE] added support to hide search result
    
    * [REFACTOR] version change
    
    * [REFACTOR]
    
    * [REFACTOR]
    
    * [REFACTOR] image selector
    
    * [REFACTOR]
    
    * [REFACTOR]
    
    * Revert "[REFACTOR]"
    
    This reverts commit d63dba80b2cd2ef2ede332f96ade40f974149bd4.
    
    * [REFACTOR]
    
    * [REFACTOR]
    
    * [FEATURE] apply blur effect instead of replacing
    
    * [REFACTOR]
    
    * version change
    
    * [REFACTOR]

--- src/spoilerPreviewFix.user.js
+++ src/spoilerPreviewFix.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Reddit: Hide Spoiler Image Previews
 // @namespace    https://github.com/SeyTi01/
-// @version      1.4
-// @description  Hides visible spoiler image previews on new Reddit.
+// @version      1.6
+// @description  Applies blur effect to visible spoiler image previews on new Reddit
 // @author       SeyTi01
 // @match        https://www.reddit.com/*
 // @grant        none
@@ -12,43 +12,38 @@
 (function() {
     'use strict';
 
-    const config = {
-        iconClass: '_3CquMWJ6RMh8E9D-_84AtZ _2hIvPRO2xz4rn9LXAJXYDa _10qSZsDWnOBwx4bc7GJ1QF icon icon-media_gallery',
-        imageSelector: 'div[aria-label][data-click-id="image"]',
-        backgroundDivSelector: 'div[data-click-id="background"]',
-        spoilerSelector: 'span._1wzhGvvafQFOWAyA157okr',
+    const SELECTORS = {
+        image: 'div._2c1ElNxHftd8W_nZtcG9zf',
+        spoiler: 'span._1wzhGvvafQFOWAyA157okr, span._1P0ASR__enq34IxkSim2Rk',
+        background: '._1poyrkZ7g36PawDueRza-J'
     };
 
-    const iconContainer = createIconContainer(config.iconClass);
+    const spoilerClass = 'GnWcY6GPzeZ5rzsiQ98fo';
     const observer = new MutationObserver(observeMutations);
 
-    function hideSpoilerImage(image) {
-        const spoilerSpan = image.closest(config.backgroundDivSelector).querySelector(config.spoilerSelector);
-        if (spoilerSpan) {
-            image.replaceWith(iconContainer.cloneNode(true));
-        }
-    }
+    document.querySelectorAll(SELECTORS.image).forEach(applySpoilerStyles);
+    observer.observe(document.body, { childList: true, subtree: true });
 
     function observeMutations(mutations) {
         for (const mutation of mutations) {
             if (mutation.addedNodes.length > 0) {
                 mutation.addedNodes.forEach((node) => {
-                    if (node.nodeType === Node.ELEMENT_NODE) {
-                        node.querySelectorAll(config.imageSelector).forEach(hideSpoilerImage);
-                    }
+                    handleAddedNode(node);
                 });
             }
         }
     }
 
-    function createIconContainer(iconClass) {
-        const container = document.createElement('div');
-        const icon = document.createElement('i');
-        icon.className = iconClass;
-        container.appendChild(icon);
-        return container;
+    function handleAddedNode(node) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            node.querySelectorAll(SELECTORS.image).forEach(applySpoilerStyles);
+        }
     }
 
-    document.querySelectorAll(config.imageSelector).forEach(hideSpoilerImage);
-    observer.observe(document.body, { childList: true, subtree: true });
+    function applySpoilerStyles(image) {
+        const spoilerSpan = image.closest(SELECTORS.background).querySelector(SELECTORS.spoiler);
+        if (spoilerSpan) {
+            image.classList.add(spoilerClass);
+        }
+    }
 })();
