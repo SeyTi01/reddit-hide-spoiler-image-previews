ec31128084922e7fc752188ea2640e0a7eaf705f
SeyTi01 <jannis.ridder@gmail.com>
2023-09-16 23:15:16 +0200

    version change

--- src/spoilerPreviewFix.user.js
+++ src/spoilerPreviewFix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit: Hide Spoiler Image Previews
 // @namespace    https://github.com/SeyTi01/
-// @version      1.5
+// @version      1.6
 // @description  Applies blur effect to visible spoiler image previews on new Reddit.
 // @author       SeyTi01
 // @match        https://www.reddit.com/*
